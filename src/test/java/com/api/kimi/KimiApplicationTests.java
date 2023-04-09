68ac8a1b03271fa3930ef92f6b4afdf61797aa90
paula <pflogar0401@g.educaand.es>
2023-04-09 22:26:35 +0200

    base

---
+++ src/test/java/com/api/kimi/KimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.kimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
